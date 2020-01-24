157d3bbaaff790f990e8230e4b3d8926da8c87fe
JBuck31 <jrbuck31@gmail.com>
2020-01-23 19:14:39 -0600

    Completed Part 2 Models

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,32 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(max = 50)
+    private String location;
 
+    public Employer(@NotBlank @Size(max = 50) String location) {
+        this.location = location;
+    }
+
+
+    //no argument constructor for hibernate
+    public Employer(){
+    }
+
+    //getters and setters
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,30 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank
+    @Size(max = 200)
+    private String description;
+
+    //constructors
+    public Skill(@NotBlank @Size(max = 200) String description) {
+        this.description = description;
+    }
+
+    public Skill() {
+    }
+
+    //getter and setter
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
